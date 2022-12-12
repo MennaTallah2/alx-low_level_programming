bf47c7868f06272f26cd511e0dd7e278adbbe2d8
your_mennatullah <your_mennaibraheem2@gmail.com>
2022-12-12 14:22:57 -0800

    comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Return :Always 0
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 8; a++)
+	{
+	for (b = 1; b <= 9; b++)
+	{
+	putchar(a % 10 + '0');
+	putchar(b % 10 + '0');
+	if (a == 8 && b == 9)
+	{
+	}
+	else
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
