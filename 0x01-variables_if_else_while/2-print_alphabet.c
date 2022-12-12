f9ed98f69e6c122d55282a65377ff97e590095e1
your_mennatullah <your_mennaibraheem2@gmail.com>
2022-12-11 18:48:25 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main-Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	char r;
+
+	for (r = 'a'; r <= 'z'; r++)
+	{
+	putchar(r);
+	}
+	printf("\n");
+	return (0);
+}
