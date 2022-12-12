37ec83bc6ce26ac9dc87354e570f27a1e81ed012
your_mennatullah <your_mennaibraheem2@gmail.com>
2022-12-11 20:10:48 -0800

    mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	char r;
+
+	for (r = 'z'; r >= 'a'; r--)
+	{
+	putchar(r);
+	}
+	putchar('\n');
+	return (0);
+
+}
