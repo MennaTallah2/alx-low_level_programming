d5c894dc6860d6788e50219869ddb47be92eca70
your_mennatullah <your_mennaibraheem2@gmail.com>
2022-12-11 19:20:32 -0800

    alphabetsWithExceptions

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main-entry
+ * return: Alwaya 0
+ */
+int main(void)
+{
+	char r;
+
+	for (r = 'a'; r <= 'z'; r++)
+	{
+	if (r == 'q' || r == 'e')
+	{}
+	else
+	{
+	putchar(r);
+	}
+	}
+	putchar('\n');
+	return (0);
+}
