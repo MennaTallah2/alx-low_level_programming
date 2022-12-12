fdbcefb5424e591c77d443458eea37d6126683ec
your_mennatullah <your_mennaibraheem2@gmail.com>
2022-12-11 16:57:18 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
-<<<<<<< HEAD
 #include <string.h>
-=======
->>>>>>> 73ea25e584ce7bf58680f8be087f2967e376b5fa
 /**
  * main - Entry
  * Return: Always 1
