a775f92e07f9573f4b764216d639ee9651c47079
your_mennatullah <your_mennaibraheem2@gmail.com>
2022-12-12 14:28:23 -0800

    comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 /**
- * main - entry
+ * main-entry
  * Return :Always 0
  */
+
 int main(void)
 {
 	int a, b;
 
 	for (a = 0; a <= 8; a++)
 	{
-	for (b = 1; b <= 9; b++)
+	for (b = a + 1; b <= 9; b++)
 	{
 	putchar(a % 10 + '0');
 	putchar(b % 10 + '0');
