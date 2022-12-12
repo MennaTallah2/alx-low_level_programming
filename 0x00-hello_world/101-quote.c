fc07e8c6a444102db092508b0dc3d3e6887aca88
your_mennatullah <your_mennaibraheem2@gmail.com>
2022-12-11 16:52:06 -0800

    betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <string.h>
 #include <sys/types.h>
-
+#include <string.h>
 /**
  * main - entry
  * Return: Always 1
  */
+
 int main(void)
 {
-	char text[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2,text,strlen(text));
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, str, strlen (str));
 	return (1);
 }
