75884740067a53cf70f10cf3bd45ee57dacc7728
your_mennatullah <your_mennaibraheem2@gmail.com>
2022-12-13 13:58:55 -0800

    alpabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * print_alphabet - prints alphabets in lowercase
+ * main-print
+ * Description: print alphabets
+ * Retun:Always 0
+ */
+
 void print_alphabet(void)
 {
 	char x;
 
-	for(x = 'a'; x <= 'z'; x++)
+	for (x = 'a'; x <= 'z'; x++)
 	{
 	_putchar(x);
 	}
--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * main- entry
+ * Description: call print alphabet
  * Return: Always 0
  */
 int main(void)
