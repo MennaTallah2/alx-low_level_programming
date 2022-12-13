bd2b2fe990a78cba92c388713c742c65b590b7d3
your_mennatullah <your_mennaibraheem2@gmail.com>
2022-12-13 10:03:15 -0800

    include

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,14 +2,14 @@
 #include "main.h"
 /**
  * main-entry
- * Description-print putchar
- * Return - Always 0
+ * Description: print putchar
+ * Return: Always 0
  */
 int main(void)
 {
 	char putchar[] = "_putchar";
 	int i = 0;
-	
+
 	while (putchar[i] != '\0')
 	{
 	_putchar(putchar[i]);
