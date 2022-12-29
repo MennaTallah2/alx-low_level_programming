9e7558289a39db93d00662855cdb0ad734259748
your_mennatullah <your_mennaibraheem2@gmail.com>
2022-12-29 14:52:40 -0800

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - update the value that the given pointer pints to
+ *
+ * @n: pointer passed to the function
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
