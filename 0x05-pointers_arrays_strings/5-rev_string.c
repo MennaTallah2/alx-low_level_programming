54250ac25250d7eb3746708bab96ebb1deec7218
your_mennatullah <your_mennaibraheem2@gmail.com>
2022-12-29 17:06:23 -0800

    rev

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,12 +1,12 @@
 #include "main.h"
-
+#include <strign.h>
 /**
  * rev_string - a function that reveses the strign
  * @s: is the pointer for the string value
  */
 void rev_string(char *s)
 {
-	int n = _strlen(s);
+	int n = strlen(s);
 	int temp, i;
 
 	for (i = 0; i < n / 2; i++)
