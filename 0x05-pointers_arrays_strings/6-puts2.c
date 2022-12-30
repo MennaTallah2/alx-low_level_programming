beecb81491292faa2f557cfa2a37e68fbdda7aa9
your_mennatullah <your_mennaibraheem2@gmail.com>
2022-12-29 17:12:40 -0800

    pusts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - function that prints a string, followed by a new line, to stdout
+ * @str: pointer value
+ */
+void puts2(char *str)
+{
+		int i;
+
+		i = 0;
+		while (str[i] != '\0')
+		{
+			if (i % 2 == 0)
+				_putchar(str[i]);
+			i++;
+		}
+		_putchar('\n');
+}
