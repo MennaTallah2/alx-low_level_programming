b2ca5b20ea35f7f26f13660a7e909d996bf63173
your_mennatullah <your_mennaibraheem2@gmail.com>
2022-12-29 16:02:58 -0800

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string passed to calc its length
+ * Rerutn: length value
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[i] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
