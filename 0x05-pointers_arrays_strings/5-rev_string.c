c9dd83333ac2d089c3576705055e6a40249ee35f
your_mennatullah <your_mennaibraheem2@gmail.com>
2022-12-29 17:01:16 -0800

    reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - a function that reveses the strign
+ * @s: is the pointer for the string value
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	int n = _strlen(*s);
+	int temp;
+	char rev;
+
+	for (int i = 0; i < n / 2; i++)
+	{
+		temp = s[n - i - 1];
+		s[n - i - 1] = s[i];
+		s[i] = temp;
+
+	}
+
+}
