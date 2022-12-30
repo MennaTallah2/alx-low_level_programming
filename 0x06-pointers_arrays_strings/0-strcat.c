e0f9031bee9e3098493f675b646dca7d3ec3f674
your_mennatullah <your_mennaibraheem2@gmail.com>
2022-12-29 17:58:41 -0800

    fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,13 +11,15 @@ char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	i = 0;
+	while (dest[i] != '\0')
 	{
-		for (j = 0; src[j] != '\0'; j++)
-		{
-			dest[i] = src[j];
-			i++;
-		}
+		i++;
+	}
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
 	}
 	dest[i] = '\0';
 	return (dest);
