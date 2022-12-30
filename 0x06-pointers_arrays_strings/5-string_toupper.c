acbae9b66821265a9119b59465bb2ee2bd491d28
your_mennatullah <your_mennaibraheem2@gmail.com>
2022-12-29 18:31:27 -0800

    toUpperCase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase letters of  a string to uppercase
+ * @word: pointer to string
+ * Return: uppercase string
+ */
+char *string_toupper(char *word)
+{
+	int len;
+
+	len = 0;
+	while (word[len] != '\0')
+	{
+		if (word[len] >= 97 && word[len] <= 122)
+		{
+			word[len] = word[len] - 32;
+		}
+		len++;
+	}
+	return (word);
+}
