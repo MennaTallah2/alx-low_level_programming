046d84bc910e0f280d4507af75ba50fbe5dfe8ce
your_mennatullah <your_mennaibraheem2@gmail.com>
2023-01-04 15:41:31 -0800

    power calc fun

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _pow_recursion - function calculate the pow
+ * @x: rhe base number
+ * @y: the power number
+ * Return: the value of x power y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y != 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
