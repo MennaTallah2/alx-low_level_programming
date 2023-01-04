ed329fe9dd6e7c4a2391f68ab03ff451681c32b5
your_mennatullah <your_mennaibraheem2@gmail.com>
2023-01-03 16:31:03 -0800

    prefix

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn - function returns the length of a string
+ * @s:number of bytes in the initial segment
+ * @accept:character to check bytes with
+ * Return: number of bytes in the initual segment of s.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+
+	i = 1;
+	while (*s != '\0')
+	{
+		while (*f != '\0')
+		{
+			if (*f == *s)
+			{
+				return (i);
+			}
+
+			f++;
+		}
+		s++;
+		i++;
+	}
+	return (i);
+}
