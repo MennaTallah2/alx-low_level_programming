b7bcf414a53626b469dfa6c8c278bcd69a4384cb
your_mennatullah <your_mennaibraheem2@gmail.com>
2023-01-03 16:08:39 -0800

    search

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - function that locates char in a string
+ * @s:the string
+ * @c:the character
+ * Return:the first occurrence of c in s or null if not found
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+
+}
