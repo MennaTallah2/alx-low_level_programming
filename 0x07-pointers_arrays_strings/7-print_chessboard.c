b7fb8ce891a50785e99271bc08caaf57554761d9
your_mennatullah <your_mennaibraheem2@gmail.com>
2023-01-03 18:24:27 -0800

    chessB

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,6 +8,9 @@ void print_chessboard(char (*a)[8])
 	int indx1, indx2;
 
 	for (indx1 = 0; a[indx1][7]; indx1++)
-		_putchar(a[indx1][indx2]);
-	_putchar('\n');
+	{
+		for (indx2 = 0; indx2 < 8; indx2++)
+			_putchar(a[indx1][indx2]);
+		_putchar('\n');
+	}
 }
