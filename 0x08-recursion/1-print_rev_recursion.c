78b7c8cbc3212b548904dcd6438f79fa5248c1f1
your_mennatullah <your_mennaibraheem2@gmail.com>
2023-01-04 15:14:48 -0800

    reverseStirng

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - function that prints string in reverse
+ * @s: pointer to the string value
+ */
+void _print_rev_recursion(char *s)
+{
+	while (*s != '\0')
+	{
+		_print_rev_recursion(s + 1)
+		_putchar(*s);
+	}
+}
