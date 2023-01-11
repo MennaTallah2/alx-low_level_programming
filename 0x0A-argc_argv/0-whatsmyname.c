5b0b5643192c686c3e28c769a2619f9b6f442ce5
your_mennatullah <your_mennaibraheem2@gmail.com>
2023-01-11 15:52:45 -0800

    fixing void

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,9 @@
  * @argv: the array comtains the arguments
  * Return: Always 0 if succes
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
+	(void) argc;
 	printf("%s\n", argv[0]);
 	return (0);
 }
