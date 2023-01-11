156a32ca41ffdbab3e639cd80c45215d95fa7c66
your_mennatullah <your_mennaibraheem2@gmail.com>
2023-01-11 15:48:37 -0800

    argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - function prints the name of the program
+ * @argc:the number of arguments
+ * @argv: the array comtains the arguments
+ * Return: Always 0 if succes
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
