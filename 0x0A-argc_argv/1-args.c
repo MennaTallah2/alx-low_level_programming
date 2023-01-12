3840727da79077421a8f369548819f1f3b5c7ed1
your_mennatullah <your_mennaibraheem2@gmail.com>
2023-01-11 16:01:15 -0800

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mian - function prints the number of arguments passed
+ * @argc: number of arguments
+ * @argv: the array of arguments passed
+ * Return: always 0 if success
+ */
+int main(int argc, int **argv)
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
